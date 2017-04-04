ca278d63490a0c4597eea0d2cdb9dddf88a61635
hiyanjob <hiyanjob2017@gmail.com>
2017-04-04 17:50:50 +0530

    Merge remote-tracking branch 'remotes/origin/Pradeep' changes in checkout login

---
+++ app/src/main/java/lodore/com/lodore/service/APISevice.java
@@ -0,0 +1,9 @@
+package lodore.com.lodore.service;
+
+/**
+ * Created by win7 on 04-Apr-17.
+ */
+
+public interface APISevice {
+
+}
---
+++ app/src/main/java/lodore/com/lodore/utils/GlobalConstant.java
@@ -0,0 +1,16 @@
+package lodore.com.lodore.utils;
+
+/**
+ * Created by win7 on 04-Apr-17.
+ */
+
+public class GlobalConstant {
+
+    public static final String base_url = "192.168.123.2/lodore/api";
+    public static final String login_url = base_url+"/customer/login";
+    public static final String update_url = base_url+"/customer/update";
+    public static final String register_url = base_url+"/customer";
+
+
+
+}
